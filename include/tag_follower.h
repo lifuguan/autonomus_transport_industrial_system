56874e1a0e5a44918bcbc7c684bd3392f870d87f
lifuguan <1002732355@qq.com>
2020-08-22 17:32:28 +0800

    完善service通信机制

--- include/tag_follower.h
+++ include/tag_follower.h
@@ -11,10 +11,11 @@
 #define TAG_FOLLOWER_
 
 #include <iostream>
+#include <string>
 #include <ros/ros.h>
 #include <tf/tf.h>
 #include <tf/transform_listener.h>
-
+#include "autonomus_transport_industrial_system/netComTag.h"
 namespace AutonomusTransportIndustrialSystem
 {
     class TagFollower
@@ -24,24 +25,42 @@ namespace AutonomusTransportIndustrialSystem
         tf::StampedTransform tag_transform;
 
         tf::TransformListener tag_listener;
+
+        const int UN_INITIALIZE = -1;
+        const int IS_DONE = -2;
+        // -1: 未初始化；0~2：目标tag
+        int tag_num = UN_INITIALIZE;
+
+        /// tag 的ros service回调函数
+        /// \param req
+        /// \param res
+        /// \return
+        bool tagServiceCallBack(autonomus_transport_industrial_system::netComTag::Request & req, autonomus_transport_industrial_system::netComTag::Response& res);
     public:
         TagFollower(ros::NodeHandle given_nh) : nh(given_nh)
         {
-            // 此处采取Service向Navigation订阅目标信息，修改tf监听值
+            // 此处采取Service向netComModule订阅目标信息，修改tf监听值
+            ros::ServiceServer tag_service = nh.advertiseService("netComTag", &TagFollower::tagServiceCallBack, this);
         }
         ~TagFollower() = default;
 
-        bool tagCallBack(const ros::TimerEvent&);
+        bool tagEventProcess(const ros::TimerEvent&);
     };
 
-    bool TagFollower::tagCallBack(const ros::TimerEvent &)
+    bool TagFollower::tagEventProcess(const ros::TimerEvent &)
     {
         // 读取tag的tf
         try
         {
-            tag_listener.lookupTransform("/base_link", "/tag_0", ros::Time(0), tag_transform);
-            ROS_INFO_STREAM("Actual position: x = " << tag_transform.getOrigin().z() << ", y = " << tag_transform.getOrigin().x());
-
+            if (tag_num != UN_INITIALIZE && tag_num != IS_DONE)
+            {
+                tag_listener.lookupTransform("/base_link", "/tag_"+std::to_string(tag_num), ros::Time(0), tag_transform);
+                ROS_INFO_STREAM("Actual position: x = " << tag_transform.getOrigin().z() << ", y = " << tag_transform.getOrigin().x());
+            }
+            else
+            {
+                ROS_INFO_STREAM("tag num is not initialize OR the mission is finished.");
+            }
         } catch (tf::TransformException &ex)
         {
             ROS_WARN_STREAM("tf lookup is not exist : " << ex.what());
@@ -49,7 +68,13 @@ namespace AutonomusTransportIndustrialSystem
         }
     }
 
-
+    bool TagFollower::tagServiceCallBack(autonomus_transport_industrial_system::netComTag::Request &req,
+                                         autonomus_transport_industrial_system::netComTag::Response &res)
+    {
+        tag_num = req.tag_num;
+        res.status = true;
+        return true;
+    }
 }
 
 
--- src/tag_follower.cpp
+++ src/tag_follower.cpp
@@ -10,7 +10,7 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "tag_follower");
     ros::NodeHandle nh;
     AutonomusTransportIndustrialSystem::TagFollower tg(nh);
-    ros::Timer tag_timer = nh.createTimer(ros::Duration(0.1), &AutonomusTransportIndustrialSystem::TagFollower::tagCallBack, &tg);
+    ros::Timer tag_timer = nh.createTimer(ros::Duration(0.1), &AutonomusTransportIndustrialSystem::TagFollower::tagEventProcess, &tg);
 
     ros::spin();
     return  EXIT_SUCCESS;
